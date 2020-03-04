572c6419b2a438c13630b8b5f45b59936598a4d7
69MalyshevAV <69MalyshevAV@region.cbr.ru>
2020-03-04 16:44:42 +0700

    add initial bundler library

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
     },
     testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.ts',
+    },
 };
